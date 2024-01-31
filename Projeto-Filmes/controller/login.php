3999dcc10a15ff0cc3cb7ee6a71472de5d552f68
Vinícius Balsemão <vinibalsemao@gmail.com>
2024-01-30 23:12:20 -0300

    Update login.php

--- Projeto-Filmes/controller/login.php
+++ Projeto-Filmes/controller/login.php
@@ -5,33 +5,35 @@ ini_set('display_errors', '1');
 include('../models/conexao.php');
 
 if (isset($_POST['email']) && isset($_POST['senha'])) {
-    if (strlen($_POST['email']) == 0) {
-        header("Location: ../views/pages/login.php");
-    } else if (strlen($_POST['senha']) == 0) {
+    if (strlen($_POST['email']) == 0 || strlen($_POST['senha']) == 0) {
         header("Location: ../views/pages/login.php");
     } else {
         $email = $mysqli->real_escape_string($_POST['email']);
         $senha = $mysqli->real_escape_string($_POST['senha']);
 
-        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-        $sql_code = $mysqli->query($sql_code) or die($mysqli->error);
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email'";
+        $result = $mysqli->query($sql_code) or die($mysqli->error);
 
-        $qtd = $sql_code->num_rows;
+        if ($result->num_rows == 1) {
+            $usuario = $result->fetch_assoc();
 
-        if ($qtd == 1) {
-            $usuario = $sql_code->fetch_assoc();
+            if (password_verify($senha, $usuario['senha'])) {
+                if (!isset($_SESSION)) {
+                    session_start();
+                }
 
-            if (!isset($_SESSION)) {
-                session_start();
+                $_SESSION['id'] = $usuario['id'];
+                $_SESSION['nome'] = $usuario['nome'];
+
+                header("Location: ../views/pages/index.php");
+            } else {
+                $_SESSION['erro'] = "Senha incorreta!";
+                header("Location: ../views/pages/login.php");
             }
-
-            $_SESSION['id'] = $usuario['id'];
-            $_SESSION['nome'] = $usuario['nome'];
-
-            header("Location: ../views/pages/index.php");
         } else {
-            $_SESSION['erro'] = "Erro ao logar!";
+            $_SESSION['erro'] = "Usuário não encontrado!";
             header("Location: ../views/pages/login.php");
         }
     }
 }
+?>
